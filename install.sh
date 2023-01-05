ea021a8983d1c7f1ac3f7d20b58c0607af324718
Muhammad Alkady <kady.muhammad@gmail.com>
2023-01-05 17:53:54 +0200

    update post installation script

--- install.sh
+++ install.sh
@@ -4,7 +4,16 @@ welcome_message
 
 # Updating ArchLinux System.
 echo "Updating ArchLinux System..."
+sudo pacman -Syy --noconfirm
 sudo pacman -Syu --noconfirm
+
+echo "Installing yay..."
+sudo git clone https://aur.archlinux.org/yay.git
+sudo chown -R cloudcone:users yay
+cd yay || exit
+makepkg -si
+yay --version
+cd ..
 
 # Installing Xorg.
 echo "Installing Xorg..."
@@ -30,7 +39,7 @@ echo "Installing network manager"
 sudo pacman -S networkmanager --noconfirm --needed
 
 echo "Installing themes..."
-sudo pacman -S lxappearance paper-icon-theme adapta-gtk-theme --noconfirm --needed
+sudo pacman -S lxappearance papirus-icon-theme adapta-gtk-theme --noconfirm --needed
 
 echo "Installing disk tools..."
 sudo pacman -S gparted ntfs-3g --noconfirm --needed
@@ -50,8 +59,33 @@ sudo pacman -S tldr wget neofetch --noconfirm --needed
 echo "Installing media tools..."
 sudo pacman -S pavucontrol mvp ffmpeg --noconfirm --needed
 
-echo "Enabling lightdm greeter"
+echo "Enabling lightdm greeter..."
 sudo systemctl enable lightdm -f
+
+echo "Installing terminal..."
+sudo pacman -S xfce4-terminal --noconfirm --needed
+
+echo "Installing terminal..."
+sudo pacman -S xfce4-terminal --noconfirm --needed
+
+echo "Installing thunar..."
+sudo pacman -S thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman --noconfirm --needed
+
+echo "Installing Iosevka and MicrosoftFonts fonts..."
+echo y | LANG=C yay --noprovides --answerdiff None --answerclean None --mflags "--noconfirm" -S ttf-ms-fonts ttc-iosevka ttf-iosevka-nerd
+
+echo "Installing Google Chrome..."
+echo y | LANG=C yay --noprovides --answerdiff None --answerclean None --mflags "--noconfirm" -S google-chrome
+
+cp dofiles/.XResources .XResources
+xrdb -merge .Xresources
+
+cp dofiles/.gtkrc-2.0.mine .gtkrc-2.0.mine
+
+mkdir .config
+cp -r i3 .config/i3
+cp -r i3status .config/i3status
+cp -r geany .config/geany
 
 # Rebooting....
 echo "Rebooting...."
