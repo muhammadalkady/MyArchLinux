02132122212ab1ad96d3303a49f9daae5205e776
Muhammad Alkady <kady.muhammad@gmail.com>
2023-01-14 11:49:50 +0200

    Update installtion scripts and add systemd service

--- install_2.sh
+++ install_2.sh
@@ -1,20 +1,46 @@
 #!/bin/bash
-# Define constants.
+
+# ----------------------------- Define constants. ---------------------------- #
+
 dotfiles_path=dotfiles
-# Copying configuration
-# git
-cp $dotfiles_path/.gitconfig "$HOME"/.gitconfig
-# XResources
-cp $dotfiles_path/.Xresources "$HOME"/.Xresources
-# GTK theme
-cp $dotfiles_path/.gtkrc-2.0.mine "$HOME"/.gtkrc-2.0
-cp -r $dotfiles_path/gtk-2.0 "$HOME"/.config/gtk-2.0
-cp -r $dotfiles_path/gtk-3.0 "$HOME"/.config/gtk-3.0
-#
+
+# --------------------------- Copying configuration -------------------------- #
+
+# ------------------------------------ git ----------------------------------- #
+
+cp $dotfiles_path/home/.gitconfig "$HOME"/.gitconfig
+
+# -------------------------------- XResources -------------------------------- #
+
+cp $dotfiles_path/home/.Xresources "$HOME"/.Xresources
+xrdb -merge .Xresources
+
+# --------------------------------- GTK theme -------------------------------- #
+
+cp $dotfiles_path/home/.gtkrc-2.0.mine "$HOME"/.gtkrc-2.0
+cp -r $dotfiles_path/home/gtk-2.0 "$HOME"/.config/gtk-2.0
+cp -r $dotfiles_path/home/gtk-3.0 "$HOME"/.config/gtk-3.0
+
+# ------------------------- Making .config directory ------------------------- #
+
 mkdir -p "$HOME"/.config
-cp -r $dotfiles_path/i3 "$HOME"/.config/i3
-cp -r $dotfiles_path/i3status "$HOME"/.config/i3status
-cp -r $dotfiles_path/geany "$HOME"/.config/geany
+
+# --------------------------------- i3 config -------------------------------- #
+
+cp -r $dotfiles_path/home/.config/i3 "$HOME"/.config/i3
+
+# ----------------------------- i3 status config ----------------------------- #
+
+cp -r $dotfiles_path/home/.config/i3status "$HOME"/.config/i3status
+
+# ------------------------- Geany text editor config ------------------------- #
+
+cp -r $dotfiles_path/home/.config/geany "$HOME"/.config/geany
+
+# ------------------------------ light dm theme ------------------------------ #
+
 sudo cp -a $dotfiles_path/dm/. /etc/lightdm/
+
+# --------------------------------- Rebooting -------------------------------- #
 
 reboot
